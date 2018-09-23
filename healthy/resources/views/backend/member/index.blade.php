da9ba7ac04bef10796564d0646790a2e5ac3efda
huongvt <huongvt@deha-soft.com>
2018-09-23 22:00:03 +0700

    index members

--- healthy/resources/views/backend/member/index.blade.php
+++ healthy/resources/views/backend/member/index.blade.php
@@ -1,4 +1,11 @@
-@extends('layouts.backend.app')
+@extends('layouts.backend.header')
 @section('content')
-<p>This is list member</p>
+    <div class="content">
+        <div class="para-header">
+            <h3>This is list member</h3>
+        </div>
+        <div class="para-content">
+
+        </div>
+    </div>
 @endsection
