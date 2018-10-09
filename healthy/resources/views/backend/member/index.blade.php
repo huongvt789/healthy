4b0b4ba4d7db3a8d11b275afd00a4beeba959a1e
huongvt <huongvt@deha-soft.com>
2018-10-09 17:48:14 +0700

    design index

--- healthy/resources/views/backend/member/index.blade.php
+++ healthy/resources/views/backend/member/index.blade.php
@@ -3,9 +3,37 @@
     <div class="content">
         <a class="para-header">
             <a href="{{route('create')}}">Add new member</a>
-        </div>
-        <div class="para-content">
+    </div>
+    <div class="para-content">
+        <div class="content-search">
 
         </div>
+        <div class="data-list">
+            <div class="table-responsive">
+                <table class="table table-hover">
+                    <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>E-mail</th>
+                        <th>Phone</th>
+                        <th>Branch</th>
+                        <th>Permission</th>
+                        <th>Action</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
     </div>
 @endsection
